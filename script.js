b73946534053a77044634b411ce2940f2e7821c4
Cameron Walden <cameronkwalden@gmail.com>
2021-06-08 10:20:01 -0700

    fixed buttons

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const game = () => {
     let pScore = 0;
     let cScore = 0;
-  
-    //Start the Game
+
+   //Starts the Game
     const startGame = () => {
       const playBtn = document.querySelector(".intro button");
       const introScreen = document.querySelector(".intro");
@@ -13,41 +13,40 @@ const game = () => {
         match.classList.add("fadeIn");
       });
     };
-    //Play Match
-    const playMatch = () => {
-      const options = document.querySelectorAll(".options button");
-      const playerCharacter = document.querySelector(".player-character");
-      const computerCharacter = document.querySelector(".computer-character");
-      const choices = document.querySelectorAll(".choices img");
-  
-      choices.forEach(choices => {
-        choices.addEventListener("animationend", function() {
-          this.style.animation = "";
-        });
-      });
-      //Computer Options
-      const computerOptions = ["rock", "paper", "scissors"];
-  
-      options.forEach(option => {
-        option.addEventListener("click", function() {
-          //Computer Choice
-          const computerNumber = Math.floor(Math.random() * 3);
-          const computerChoice = computerOptions[computerNumber];
-  
-          setTimeout(() => {
-            //Here is where we call compareCharacters
-            compareCharacters(this.textContent, computerChoice);
-            //Update Images
-            playerCharacter.src = `assets/${this.textContent}.png`;
-            computerCharacter.src = `assets/${computerChoice}.png`;
-          }, 2000);
+    //Plays a match
+    function playMatch() {
+        const options = document.querySelectorAll(".options button");
+        const playerChar = document.querySelector(".player-character");
+        const computerChar = document.querySelector(".computer-character");
+        const characters = document.querySelectorAll(".characters img");
 
-          //animation to move characters when chosen
-          playerCharacter.style.animation = "shakePlayer 0.5s";
-          computerCharacter.style.animation = "shakeComputer 0.5s";
+        characters.forEach(characters => {
+            characters.addEventListener("animationend", function () {
+                this.style.animation = "";
+            });
         });
-      });
-    };
+        //Computer Options
+        const computerOptions = ["rock", "paper", "scissors"];
+
+        options.forEach(option => {
+            option.addEventListener("click", function () {
+                //Computer Choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+                setTimeout(() => {
+                    //Here is where we call comparechar
+                    compareChar(this.textContent, computerChoice);
+                    //Update Images
+                    playerChar.src = `assets/${this.textContent}.png`;
+                    computerChar.src = `assets/${computerChoice}.png`;
+                }, 2000);
+                //Animation
+                playerChar.style.animation = "shakePlayer 0.5s ease";
+                computerChar.style.animation = "shakeComputer 0.5s ease";
+            });
+        });
+    }
   
     const updateScore = () => {
       const playerScore = document.querySelector(".player-score p");
@@ -56,15 +55,15 @@ const game = () => {
       computerScore.textContent = cScore;
     };
   
-    const compareCharacters = (playerChoice, computerChoice) => {
-      //Update Text
+    const compareChar = (playerChoice, computerChoice) => {
+     
       const winner = document.querySelector(".winner");
-      //Checking for a tie
+      
       if (playerChoice === computerChoice) {
         winner.textContent = "Tie!";
         return;
       }
-      //Check for Rock
+      
       if (playerChoice === "rock") {
         if (computerChoice === "scissors") {
           winner.textContent = "You win!";
@@ -78,7 +77,7 @@ const game = () => {
           return;
         }
       }
-      //Check for Paper
+    
       if (playerChoice === "paper") {
         if (computerChoice === "scissors") {
           winner.textContent = "Computer Wins! You suck!";
@@ -92,7 +91,7 @@ const game = () => {
           return;
         }
       }
-      //Check for Scissors
+      
       if (playerChoice === "scissors") {
         if (computerChoice === "rock") {
           winner.textContent = "Computer Wins! You suck!";
@@ -108,15 +107,12 @@ const game = () => {
       }
     };
   
-    //Is call all the inner function
     startGame();
     playMatch();
   };
   
-  //start the game function
   game();
 
-// music for the page
-  window.onload = function() {
-    document.getElementById("assets/conker.mp3").play();
-}
+  
+
+  
